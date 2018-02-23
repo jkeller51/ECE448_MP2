3568ad05bdd9a2f301ee38b096cede5303b25cf9
HaokunLi1994 <35938271+HaokunLi1994@users.noreply.github.com>
2018-02-23 16:12:26 -0600

    Completed

---
+++ agent.py
@@ -0,0 +1,61 @@
+#!usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+from board import Board
+
+
+class Agent(object):
+    """ 
+    Meta-class of all agents.
+    """
+    def __init__(self, color):
+        self.color = color
+        if color == 'red':
+            self.opponent_color = 'blue'
+        else:
+            self.opponent_color = 'red'
+        self.count_step = 0
+        self.steps = []
+
+        # Initialize agent steps
+        if color == 'red':
+            for i in range(ord('a'), ord('z')+1):
+                self.steps.append(chr(i))
+        elif color == 'blue':
+            for i in range(ord('A'), ord('Z')+1):
+                self.steps.append(chr(i))
+
+    def choose_move(self, moves):
+        """ Choose a move from a list, which follows all rules.
+
+        Args:
+            moves(list): list of tuples, each tuple is a coordinate pair
+        Returns:
+            best_move(tuple)
+        """
+        temp = list(set(moves))
+        temp.sort(key=lambda x: (x[1], -1*x[0]))
+        best_move = temp[0]
+        return best_move
+
+    def make_move(self, position, gameboard):
+        """
+        Make a move
+
+        Args:
+            position(tuple): coordinates of an intersection
+            gameboard(Board): game board
+        Returns:
+            (None)
+        """
+        x_pos = position[0]
+        y_pos = position[1]
+        char = self.steps[0]
+        self.count_step += 1
+        gameboard.mark(x_pos, y_pos, char)
+
+        # Remove this marker
+        self.steps.remove(char)
+
+        #print('{0} agent {1}-th move completed!'.format(self.color, self.count_step))
---
+++ board.py
@@ -0,0 +1,214 @@
+#!usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+class Board(object):
+    """
+    A class used to represent game board of Gomoku.
+    """
+    def __init__(self, width=7, height=7):
+        self.width = width
+        self.height = height
+        
+        # Initialize game board
+        self.board = []
+        for i in range(height):
+            line = []
+            for j in range(width):
+                line.append('.')
+            self.board.append(line)
+
+    def copy(self):
+        """Copy function."""
+        temp = Board(self.width, self.height)
+        temp.board = []
+        for line in self.board:
+            temp.board.append(line.copy())
+        return temp
+
+    def terminate(self):
+        """ Check if game terminates"""
+        if self.check_tie() or self.check_win():
+            return True
+        else:
+            return False
+
+    def check_tie(self):
+        """
+        Check if current state is a tie.
+
+        Args: (None)
+        Returns: (Boolean)
+        """
+        for line in self.board:
+            if '.' in line:
+                return False
+        return True
+
+    def check_win(self):
+        """
+        Check if either side wins
+
+        Args: (None)
+        Returns: (Boolean)
+        """
+        situation = [['red'] * 5,
+                     ['blue'] * 5]
+        
+        for x in range(self.height):
+            for y in range(self.width):
+                position = (x, y)
+                h = self.check_horizontal_state(position)
+                if h in situation:
+                    return True
+
+                v = self.check_vertical_state(position)
+                if v in situation:
+                    return True
+
+                d1 = self.check_diag_1_state(position)
+                if d1 in situation:
+                    return True
+
+                d2 = self.check_diag_2_state(position)
+                if d2 in situation:
+                    return True
+
+        return False
+
+    def check_horizontal_state(self, position):
+        """
+        Check horizontal state for a position →
+
+        Args:
+            position(tuple): coordinates
+        Returns:
+            state(list): x for space
+                         eg. ['red', 'red', 'x', 'blue', 'red']
+        """
+        state = []
+        x_pos = position[0]
+        y_pos = position[1]
+
+        for i in range(y_pos, y_pos + 5):
+            # not out of range
+            if i >= self.width:
+                break
+            
+            char = self.board[x_pos][i]
+            if char.islower():
+                state.append('red')
+            elif char.isupper():
+                state.append('blue')
+            else:
+                state.append('x')
+        return state
+
+    def check_vertical_state(self, position):
+        """
+        Check vertical state for a position ↓
+
+        Args:
+            position(tuple): coordinates
+        Returns:
+            state(list): x for space
+                         eg. ['red', 'red', 'x', 'blue', 'red']
+        """
+        state = []
+        x_pos = position[0]
+        y_pos = position[1]
+
+        for i in range(x_pos, x_pos + 5):
+            # not out of range
+            if i >= self.height:
+                break
+            
+            char = self.board[i][y_pos]
+            if char.islower():
+                state.append('red')
+            elif char.isupper():
+                state.append('blue')
+            else:
+                state.append('x')
+        return state
+
+    def check_diag_1_state(self, position):
+        """
+        Check diagonal state for a position from top left
+        to bottom right ↘
+
+        Args:
+            position(tuple): coordinates
+        Returns:
+            state(list): x for space
+                         eg. ['red', 'red', 'x', 'blue', 'red']
+        """
+        state = []
+        x_pos = position[0]
+        y_pos = position[1]
+
+        for i in range(5):
+            # not out of range
+            if (x_pos + i >= self.height) or (y_pos + i >= self.width):
+                break
+            
+            char = self.board[x_pos + i][y_pos + i]
+            if char.islower():
+                state.append('red')
+            elif char.isupper():
+                state.append('blue')
+            else:
+                state.append('x')
+        return state
+
+    def check_diag_2_state(self, position):
+        """
+        Check diagonal state for a position from top right
+        to bottom left ↙
+
+        Args:
+            position(tuple): coordinates
+        Returns:
+            state(list): x for space
+                         eg. ['red', 'red', 'x', 'blue', 'red']
+        """
+        state = []
+        x_pos = position[0]
+        y_pos = position[1]
+
+        for i in range(5):
+            # not out of range
+            if (x_pos + i >= self.height) or (y_pos - i < 0):
+                break
+            
+            char = self.board[x_pos + i][y_pos - i]
+            if char.islower():
+                state.append('red')
+            elif char.isupper():
+                state.append('blue')
+            else:
+                state.append('x')
+        return state
+
+    def mark(self, x, y, char):
+        """
+        Mark an intersection by coordinates.
+
+        Args:
+            x(int): x-th line
+            y(int): y-th position
+            char(char): symbol used to mark
+        Returns:
+            (None)
+        """
+        self.board[x][y] = char
+
+    def print_board(self):
+        """
+        Print out game board
+
+        Args: (None)
+        Returns: (None)
+        """
+        for line in self.board:
+            print(''.join(line))
