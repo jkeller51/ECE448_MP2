aa4e8cdef44002b08ff83b0c56f9af499185f279
HaokunLi1994 <35938271+HaokunLi1994@users.noreply.github.com>
2018-03-01 21:40:50 -0600

    Stochastic agent is slow!!!!

---
+++ Part2_3.py
@@ -0,0 +1,72 @@
+#!usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+import time
+from board import Board
+from agent import Agent
+from reflex import Reflex
+from minimax import Minimax
+from alpha_beta import AlphaBeta
+from stochastic import Stochastic
+
+
+if __name__ == '__main__':
+    start = time.time()
+    
+    # Initialize game board
+    gameboard = Board()
+
+    # Initialize red agent
+    idx = input('Please choose a type for RED:\n'
+                '1. Stochastic\n'
+                '2. Alpha Beta\n')
+    if idx == '1':
+        RED = Stochastic('red')
+    elif idx == '2':
+        RED = AlphaBeta('red')
+
+    # Initialize blue agent
+    idx = input('Please choose a type for BLUE:\n'
+                '1. Stochastic\n'
+                '2. Alpha Beta\n')
+    if idx == '1':
+        BLUE = Stochastic('blue')
+    elif idx == '2':
+        BLUE = AlphaBeta('blue')
+
+    # Mark order or two agents
+    _red_ = 1
+    _blue_ = 0
+
+    # Count number of moves
+    red_moves = 0
+    blue_moves = 0
+
+    # Play
+    while (RED.win_lose_tie(gameboard) == 'UNFINISHED'):
+        if _red_ == 1:
+            pos = RED.find_move(gameboard)
+            RED.make_move(pos, gameboard)
+            red_moves += 1
+            print('red {0}-th move \t {1} nodes expanded'.format(
+                   red_moves, RED.expanded_nodes))
+            RED.expanded_nodes = 0
+
+        if _blue_ == 1:
+            pos = BLUE.find_move(gameboard)
+            BLUE.make_move(pos, gameboard)
+            blue_moves += 1
+            print('blue {0}-th move \t {1} nodes expanded'.format(
+                   blue_moves, BLUE.expanded_nodes))
+            BLUE.expanded_nodes = 0
+
+        _red_, _blue_ = _blue_, _red_
+
+    # Output
+    print()
+    gameboard.print_board()
+    end = time.time()
+    print()
+    print('Time used: {0:.3f} minutes.'.format((end - start) / 60))
+    print('RED {0}!'.format(RED.win_lose_tie(gameboard)))
---
+++ stochastic.py
@@ -0,0 +1,191 @@
+#!usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+from board import Board
+from agent import Agent
+from reflex import Reflex
+import numpy as np
+
+
+class Stochastic(Agent):
+    """ Inherit from class 'Agent', used to represent an agent."""    
+    def __init__(self, color):
+        super().__init__(color)
+        self.expanded_nodes = 0
+        self.type = 'stochastic'
+
+    def _policy(self, gameboard):
+        """ Find next move in simulation
+
+        Args:
+            gameboard(Board): game board
+        Returns:
+            best_move(tuple): coordinate
+        """
+        valid_moves = self._all_valid_moves(gameboard)
+        _reflex_ = Reflex(self.color)
+        best_move = None
+        moves = []
+        
+        # step 1, check going to win
+        for position in valid_moves:
+            temp = _reflex_.check_going_to_win(position, gameboard)
+            if len(temp) != 0:
+                moves += temp
+
+        if len(moves) > 0:
+            idx = np.random.choice(len(moves), 1)[0]
+            best_move = moves[idx]
+            return best_move
+        
+        # step 2, check opponent 4
+        for position in valid_moves:
+            temp = _reflex_._alter_check_opponent_4(position, gameboard)
+            if len(temp) != 0:
+                moves += temp
+        
+        if len(moves) > 0:
+            idx = np.random.choice(len(moves), 1)[0]
+            best_move = moves[idx]
+            return best_move
+
+        # step 3, check opponent 3
+        for position in valid_moves:
+            temp = _reflex_.check_opponent_3(position, gameboard)
+            if len(temp) != 0:
+                moves += temp
+        
+        if len(moves) > 0:
+            idx = np.random.choice(len(moves), 1)[0]
+            best_move = moves[idx]
+            return best_move
+
+        # step 4, winning blocks
+        for position in valid_moves:
+            temp = _reflex_.check_winning_blocks(position, gameboard)
+            if len(temp) != 0:
+                moves += temp
+
+        if len(moves) > 0:
+            moves = list(set(moves))
+            moves.sort(key=lambda x: x[2], reverse=True)
+            max_count = moves[0][2]
+            new_moves = []
+
+            for t in moves:
+                if t[2] < max_count:
+                    break
+                else:
+                    new_moves.append((t[0], t[1]))
+
+            moves = new_moves.copy()
+
+        if len(moves) > 0:
+            idx = np.random.choice(len(moves), 1)[0]
+            best_move = moves[idx]
+            return best_move
+
+        # step 5, random pick one
+        idx = np.random.choice(len(valid_moves), 1)[0]
+        return valid_moves[idx]
+        
+
+    def simulation(self, gameboard, N):
+        """
+        Generate N random games using policy, and return average value.
+
+        Args:
+            gameboard(Board): game board
+            N(int): # of games
+        Returns:
+            avg_score: average evaluation score
+        """
+        score = 0
+
+        # Simulation N times
+        for i in range(N):
+            depth = 0
+            gameboard_cpy = gameboard.copy()
+            
+            # Create two temp angents for random moves
+            _SELF_ = Stochastic(self.color)
+            _OPPONENT_ = Stochastic(self.opponent_color)
+
+            _SELF_ACTION_ = 1
+            _OPPONENT_ACTION_ = 0
+
+            while ((_SELF_.win_lose_tie(gameboard_cpy) == 'UNFINISHED')
+                   and (depth <= 12)):
+                if _SELF_ACTION_ == 1:
+                    temp = _SELF_._policy(gameboard_cpy)
+                    _SELF_.make_move(temp, gameboard_cpy)
+
+                if _OPPONENT_ACTION_ == 1:
+                    temp = _OPPONENT_._policy(gameboard_cpy)
+                    _OPPONENT_.make_move(temp, gameboard_cpy)
+
+                _SELF_ACTION_, _OPPONENT_ACTION_ = _OPPONENT_ACTION_, _SELF_ACTION_
+
+                depth += 1
+
+            result = self.win_lose_tie(gameboard_cpy)
+            if result == 'win':
+                score += 1.0
+            elif result == 'lose':
+                score += 0.0
+            elif result == 'tie':
+                score += 0.5
+            elif depth > 12:
+                score += 0.1
+        
+        avg_score = score / N
+        return avg_score
+
+    def find_move(self, gameboard, N=100):
+        """
+        Find next move
+
+        Args:
+            gameboard(Board): game board
+            N(int): number of simulations for each leaf
+        Returns:
+            best_move(tuple): coordinate to place a stone
+        """
+        # find all valid moves
+        moves = self._all_valid_moves(gameboard)
+
+        # max-agent, depth=1
+        best_move = None
+        best_score = float('-inf')
+        beta = float('-inf')
+        for pos in moves:
+            temp_board = gameboard.copy()
+            temp_agent = Agent(self.color)
+            temp_agent.make_move(pos, temp_board)
+            temp_moves = self._all_valid_moves(temp_board)
+
+            # min-agent, depth=2
+            min_move = None
+            min_score = float('inf')
+            for pos_2 in temp_moves:
+                temp_board_2 = temp_board.copy()
+                temp_agent_2 = Agent(self.opponent_color)
+                temp_agent_2.make_move(pos_2, temp_board_2)
+                
+                score = self.simulation(temp_board_2, N)
+                self.expanded_nodes += 1
+
+                if score < min_score:
+                    min_move = pos_2
+                    min_score = score
+                if score < beta:
+                    break
+
+            if min_score > best_score:
+                best_move = pos
+                best_score = min_score
+            if min_score > beta:
+                beta = min_score
+        
+        return best_move
