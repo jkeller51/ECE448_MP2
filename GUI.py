b41764ab83c1f1cf06f764db9c1fc6dff02173e5
Chinny Emeka <Chinny Emeka>
2018-03-03 15:22:23 -0600

    Added code to highlight computer's move

--- GUI.py
+++ GUI.py
@@ -13,7 +13,7 @@ class GomokuButton(Button):
 
 
 class GameManager():
-    def __init__(self, gameboard, buttons, label):
+    def __init__(self, gameboard, buttons, buttonColor,  label):
         self.board = gameboard
         self.gameStarted = True #only allow buttons and other things to work if game has officially started. 
         self.playerTurn = True #it's the human player's turn to play.
@@ -22,6 +22,7 @@ class GameManager():
         self.gameSquares = buttons
         self.gameLabel = label #label to give updates to the player. 
         self.gameDone = False
+        
        
 
         #set the computer's color.
@@ -32,19 +33,28 @@ class GameManager():
             computerColor = "red"
             
         self.computerAgent = AlphaBeta(computerColor)
-
+        self.computerPreviousSquare  = None #the last square the computer played on. 
+        self.originalBackgroundColor = buttonColor #refers to button colors
 
 
 
     def playRound(self, event):
+       print(self.originalBackgroundColor)
 
        #we check to see if game is done before either player is allowed to move
        if (self.gameDone == False):
             self.playerMove(event)
             self.gameDone = self.isGameDone() #check if game is finished
-
+       
 
        if (self.gameDone == False):
+           
+            if (self.computerPreviousSquare != None):
+                #if comp made a previous move which was highlighted, remove the highlight
+                #comp is about to make a fresh move.
+                self.computerPreviousSquare.config(background = self.originalBackgroundColor)
+
+
             self.computerMove()
             self.gameDone = self.isGameDone()
 
@@ -90,6 +100,8 @@ class GameManager():
         """
         
         buttonClicked = event.widget
+
+        
         
         if self.playerTurn == True:
             #change both internal and external state
@@ -103,16 +115,18 @@ class GameManager():
                 buttonClicked.config(text= "X") #change the external representation (text displayed)
                 self.playerTurn = False #now it's the computer's turn to play.
         
-
+           
+                
 
         #after a player has moved, check to see if someone won or tied. Display message accordingly
 
 
     def computerMove(self):
         if self.playerTurn == False: #it's the computer's turn to play
+
             
             self.gameLabel.config(text= "The computer is thinking...")
-           
+        
             compMove = self.computerAgent.find_move(self.board, depth = 2)
             self.computerAgent.make_move(compMove, self.board)
             moveID = "{x}+{y}".format(x = compMove[0], y =  compMove[1])
@@ -122,7 +136,9 @@ class GameManager():
             for square in self.gameSquares: #a list of buttons, where each button represents a game cell
                 if square.widgetID == moveID:
                     #this is the location the computer wants to play at.
-                    square.config(text = "O")
+                    square.config(text = "O", background="green")
+
+                    self.computerPreviousSquare = square
                     self.playerTurn = True
                     self.gameLabel.config(text = "Your turn to move")
             
@@ -165,17 +181,20 @@ class Window(Frame):
         self.gameStatusLabel = Label(self.master, text="Play your first move")
 
         buttonList = [] #list to store buttons for game
-
+        buttonColor = GomokuButton(self.master, textVal = "", wName = "").cget("bg") #need default color of buttons
         
         self.gameBoard = Board()
-        self.manager = GameManager(self.gameBoard, buttonList, self.gameStatusLabel) #this is the game manager
+        self.manager = GameManager(self.gameBoard, buttonList, buttonColor,  self.gameStatusLabel) #this is the game manager
         
 
         self.gameStatusLabel.grid(row = 0, column = 0)
        
+
       
+        
+        
         #buttons representing the boxes of the game
-        for i in range(self.gameBoard.height): #for each row
+        for i in range(self.gameBoard.height): #for each row  
             for j in range(self.gameBoard.width): #for each column
                 test = "{},{}".format(i, j)
                 widgetName = "{row}+{column}".format(row = i, column = j) #used as unique identifier for each button
@@ -183,8 +202,8 @@ class Window(Frame):
                 buttonList.append(newButton)
                 newButton.bind('<Button-1>', self.manager.playRound)
                 newButton.grid(row = i + 1, column = j + 1 )
-                
-           
+
+        
                 
                
 
